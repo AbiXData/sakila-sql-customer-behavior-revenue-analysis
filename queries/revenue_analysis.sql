84c4fbfe4de02eaeab02d0af243eb3340b98901e
Abiola <86834830+AbiXData@users.noreply.github.com>
2026-06-22 05:09:26 +0100

    Update revenue_analysis.sql

--- queries/revenue_analysis.sql
+++ queries/revenue_analysis.sql
@@ -49,11 +49,10 @@ FROM payment;
 -- =========================================
 -- Running total revenue
 -- =========================================
-WITH cust_running_total AS (SELECT concat(c.first_name, ' ', c.last_name) AS customer_name, SUM(p.amount)
-OVER(ORDER BY amount desc) AS running_total
+WITH customer_totals AS (SELECT CONCAT(c.first_name, ' ', c.last_name) AS customer_name, SUM(p.amount) AS total_spent
 FROM customer c
-JOIN payment p
-ON c.customer_id = p.customer_id)
-SELECT customer_name, running_total
-FROM cust_running_total
-WHERE running_total > 150;
+JOIN payment p ON c.customer_id = p.customer_id
+GROUP BY c.customer_id, c.first_name, c.last_name)
+SELECT customer_name, total_spent, SUM(total_spent) OVER (ORDER BY total_spent DESC) AS running_total
+FROM customer_totals
+ORDER BY total_spent DESC;
